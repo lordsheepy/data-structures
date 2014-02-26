f0df873bd1dbe19437e81597610ade74fbbd32be
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-25 22:26:04 -0800

    Structure for HashTable

--- hash_table.py
+++ hash_table.py
@@ -2,8 +2,40 @@
 
 
 class HashTable(object):
-    pass
+
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.buckets = []
+        for i in range(maxsize):
+            self.buckets.append(Bucket(i))
+
+    def myhash(self, key):
+        hkey = 0
+        for i in key:
+            hkey += ord(i)
+        return hkey % self.maxsize
+
+    def get(self, key):
+        hkey = HashTable.myhash(key)
+        for i in self.buckets[hkey]:
+            if key in i:
+                return i[1]
+        raise IndexError
+
+    def set(self, key, value):
+        hkey = HashTable.myhash(key)
+        try:
+            self.buckets[hkey].add((str(key), value))
+        except TypeError:
+            print "Key must be a string"
+        return
 
 
 class Bucket(object):
-    pass
+
+    def __init__(self, ident):
+        self.id = ident
+        self.contents = []
+
+    def add(self, kvpair):
+        self.contents.append(kvpair)
