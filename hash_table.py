9c7a0d8fa7480f0174ac9a086d31f2c6c4e9efa1
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-25 21:04:03 -0800

    Add hash_table.py and test_hash.py

---
+++ hash_table.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+
+class HashTable(object):
+    pass
+
+
+class Bucket(object):
+    pass
---
+++ test_hash.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class GetTest(unittest.TestCase):
+    pass
+
+
+class SetTest(unittest.TestCase):
+    pass
+
+
+class HashTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
